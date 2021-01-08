86cc3ae2d361f696c4129f89fd05d7fd06b6d9de
aguiiarlucas <lucascostaaguiar.com>
2021-01-08 17:08:32 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,6 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
-
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,19 +9,38 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.Order.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
-	
+
 	@Autowired
 	private OrderRepository repository;
-	
-	@Transactional(readOnly = true)
-	public List<OrderDTO> findall(){
-		List<Order> list = repository.findOrdersWithProducts();
-		return list.stream().map(x-> new OrderDTO(x)).collect(Collectors.toList());
-	}
-}
 
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findall() {
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional // novo pedido 
+	public OrderDTO insert(OrderDTO dto) { // instanciamos novo objetivo tipo Order com os valores abaixo.
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) { // associnado cada produto com o pedido
+			Product product = productRepository.getOne(p.getId()); // getOne = instancia o produto e não vai no BD ,
+			order.getProducts().add(product);
+		}
+		order = repository.save(order); // salva o produto no banco
+		return new OrderDTO(order); //retona obj convertido 
+	}
+
+}

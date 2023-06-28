3b4b0614e560f7e47f038bec6f0972dd83214bce
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2023-06-27 21:03:42 -0300

    BackEnd

--- front-web/src/Orders/helpers.tsx
+++
@@ -1,6 +0,0 @@
-import { Product } from "./types";
-
-export function checkIsSelected(selectedProducts : Product[],product:Product) {
-    return selectedProducts.some(item => item.id === product.id);
-
-}
